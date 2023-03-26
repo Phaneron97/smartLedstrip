d380403b6575c2e94d3eb6a78e09318f217a9e47
Christian <christian.w@hotmail.nl>
2023-03-26 22:03:09 +0200

    ledtest for testing leds (duh)

--- ledTest.py
+++ ledTest.py
@@ -1,18 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 
-ledPin = 12
+Red = 12
+Green = 13
+Blue = 18
 
 GPIO.setmode(GPIO.BCM) # Set the pin numbering scheme
-GPIO.setup(ledPin, GPIO.OUT) # Set up pin 12 as an output pin
+GPIO.setup(Red, GPIO.OUT) # Set up pin 12 as an output pin
+GPIO.setup(Green, GPIO.OUT) # Set up pin 12 as an output pin
+GPIO.setup(Blue, GPIO.OUT) # Set up pin 12 as an output pin
 
 
-while 1:
-    GPIO.output(ledPin, GPIO.HIGH)
-    print("GPIO", ledPin, "ON")
-    time.sleep(1)
-    GPIO.output(ledPin, GPIO.LOW)
-    print("Led OFF")
-    time.sleep(1)
+for i in range(3):
+    GPIO.output(Red, GPIO.HIGH)
+    print("Red ", Red, "ON")
+    time.sleep(1)    
+    GPIO.output(Red, GPIO.LOW)
+    print("Red ", Red, "Off")
+    time.sleep(1)   
+
+    GPIO.output(Green, GPIO.HIGH)
+    print("Green ", Green, "ON")
+    time.sleep(1)    
+    GPIO.output(Green, GPIO.LOW)
+    print("Green ", Green, "Off")
+    time.sleep(1)   
+
+    GPIO.output(Blue, GPIO.HIGH)
+    print("Blue ", Blue, "ON")
+    time.sleep(1)    
+    GPIO.output(Blue, GPIO.LOW)
+    print("Blue ", Blue, "Off")
+    time.sleep(1)   
+
+
+GPIO.output(Red, GPIO.LOW)
+GPIO.output(Green, GPIO.LOW)
+GPIO.output(Blue, GPIO.LOW)
+GPIO.cleanup()
+
+
+
 
 
