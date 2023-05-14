43cee1e4c3863cb8ca3f99004f14ea59c14a5a03
Christian <christian.w@hotmail.nl>
2023-05-14 15:59:26 +0200

    new file to test ledstrip turning on and off

--- randomTest.py
+++ randomTest.py
@@ -1,8 +1,25 @@
-import random
+import RPi.GPIO as GPIO
+import time
 
-minBrightnessRed = 40
-maxBrightnessRed = 100
+red_pin = 12
+green_pin = 13
+blue_pin = 18
 
-for i in range(100):
-    red_duty_cycle = random.uniform(random.uniform(minBrightnessRed-5, minBrightnessRed+5), random.uniform(maxBrightnessRed-5, maxBrightnessRed))
-    print(red_duty_cycle)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(blue_pin, GPIO.OUT)
+
+pause = 0.5
+
+while True:
+    GPIO.output(red_pin, GPIO.HIGH)    
+    time.sleep(pause)
+    GPIO.output(red_pin, GPIO.LOW)
+    GPIO.output(green_pin, GPIO.HIGH)    
+    time.sleep(pause)
+    GPIO.output(green_pin, GPIO.LOW)
+    GPIO.output(blue_pin, GPIO.HIGH)    
+    time.sleep(pause)
+    GPIO.output(blue_pin, GPIO.LOW)
+    
