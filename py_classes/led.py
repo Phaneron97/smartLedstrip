762b770a6e9b82ba1bcb8a5582f24f3a3195079d
Christian <christian.w@hotmail.nl>
2023-05-17 22:15:07 +0200

    removed inheritance support to clean up class led

--- py_classes/led.py
+++ py_classes/led.py
@@ -56,22 +56,10 @@ import pigpio
 import time
 
 class LED:
-    pwm_objects = {}
-
     def __init__(self, pin):
         self.pin = pin
         self.pi = pigpio.pi() # initialize pigpio
         self.pi.set_mode(self.pin, pigpio.OUTPUT) # set pin as output
-        
-        # create PWM object for pin, start with dutycycle of 0
-        # assign pin to pwm list if it's not in list yet
-        if self.pin not in LED.pwm_objects:
-            self.pwm = self.pi.set_PWM_dutycycle(self.pin, 0) # starting dutycycle
-            LED.pwm_objects[self.pin] = self.pwm
-        else:
-            self.pwm = LED.pwm_objects[self.pin]
-        self.current_duty_cycle = 0 # starting dutycycle
-        self.current_frequency = 100 # starting frequency
 
     def set_duty_cycle(self, duty_cycle):
         self.pi.set_PWM_dutycycle(self.pin, duty_cycle)
