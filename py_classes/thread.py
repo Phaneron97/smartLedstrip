3a1c60c66c41fe8b5c939ce2986102d9cff6fee5
Christian <christian.w@hotmail.nl>
2023-06-13 08:35:00 +0200

    updated comments

--- py_classes/thread.py
+++ py_classes/thread.py
@@ -12,8 +12,10 @@ class ThreadManager(threading.Thread):
         super().start()
 
     def terminate(self, timeout=None): # main thread waits until the thread join is called on has finished executing
+        print("thread will be joined after loop ends")
         self._running.clear()
         super().join(timeout)
 
-    def alive(self): # Check if thread is running
+    def alive(self): # Check if thread is running\
+        print("current thread is alive")
         return super().is_alive()
